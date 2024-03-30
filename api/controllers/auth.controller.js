404b1f34da6f758ce5c5f9ee2b7d3a4a5c4fed65
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-03-30 23:39:25 +0530

    create sign up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
         const { username, email, password } = req.body;
 
         if (!username || !email || !password || username === "" || email === "" || password === "") {
-            return res.status(400).json({ error: "All fields are required" });
+            next(errorHandler(400, 'All fields are required'));
         }
 
         const hashPassword = bcryptjs.hashSync(password, 10);
@@ -20,7 +20,7 @@ export const signup = async (req, res) => {
             await newUser.save();
             res.json('Signup success');
         }catch(err){
-            return res.status(400).json({ error: "Email already exists" });
+            next(err);
         }
  
 }    
