b420bc0c9af91d338e0ae9ec755c867cf0a4b99f
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-03-31 10:59:10 +0530

    create signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import Express from "express";
-import { signup } from "../controllers/auth.controller.js";
+import { signin, signup } from "../controllers/auth.controller.js";
 
 const router = Express.Router();
 
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,7 +1,7 @@
-export const errorHandler = (statuscode, message) => {
+export const errorHandler = (statusCode, message) => {
     
     const error = new Error();
-    error.statuscode = statuscode;
+    error.statusCode = statusCode;
     error.message = message;
     return error;
 };
