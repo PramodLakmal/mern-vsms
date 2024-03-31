37c319faa569ac986540d336153bd05c22a391ba
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-04-01 00:17:12 +0530

    All Complete signin/ signup front and backend and Profile UI

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,33 @@
+import {Sidebar} from 'flowbite-react';
+import {HiUser, HiArrowSmRight} from 'react-icons/hi';
+import {Link, useLocation} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+
+
+export default function DashSidebar() {
+    const location = useLocation();
+    const [tab, setTab] = useState('');
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const tabFromUrl = urlParams.get('tab');
+      if(tabFromUrl) {
+        setTab(tabFromUrl);
+      }
+    }, [location.search]); 
+  return (
+    <Sidebar className='w-full md:w-56'>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Link to='/dashboard?tab=profile'>
+                <Sidebar.Item active= {tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                    Profile
+                </Sidebar.Item>
+                </Link>
+                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
