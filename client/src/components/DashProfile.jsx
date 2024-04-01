1ed709112755928aa1af6dff3b5828831854d2a5
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-04-01 13:03:31 +0530

    complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,88 @@
-import { Button, TextInput } from 'flowbite-react';
+import { Alert, Button, TextInput } from 'flowbite-react';
 import {useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 export default function DashProfile() {
     const {currentUser} = useSelector(state => state.user);
+    const [imageFile, setImageFile] = useState(null);
+    const [imageFileUrl, setImageFileUrl] = useState(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+    const [imageFileUploadError, setImageFileUploadError] = useState(null);
+    console.log(imageFileUploadProgress, imageFileUploadError);
+    const filePickerRef = useRef();
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL(file));
+        }
+        setImageFile(e.target.files[0]);
+    };
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile]);
+
+    const uploadImage = async () => {
+      
+      /*service firebase.storage {
+        match /b/{bucket}/o {
+          match /{allPaths=**} {
+            allow read;
+            allow write: if
+            request.resource.size < 2 * 1024 * 1024 &&
+            request.resource.contentType.matches('image/.*')
+          }
+        }
+      }*/
+
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + imageFile.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
+      uploadTask.on("state_changed", snapshot => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+        
+      }, error => {
+        setImageFileUploadError('Error uploading image (File must be less than 2MB)');
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      }, () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      });
+    }
+    
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
         <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
         <form className='flex flex-col gap-4'>
-            <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-            <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightGray]' />
-            </div>   
+          <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+            <div className='relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=> filePickerRef.current.click()}>
+              {imageFileUploadProgress && (
+                <CircularProgressbar value={imageFileUploadProgress || 0} text={`${imageFileUploadProgress}%`} strokeWidth={5}
+                styles={
+                  {
+                    root: {width: '100%', height: '100%', position: 'absolute', top: 0, left: 0},
+                    path: {stroke: 'rgba(62, 152, 199, ${imageFileUploadProgress / 100 })'},
+                  }
+                }/>
+                )}
+            <img src={imageFileUrl || currentUser.profilePicture} alt="user" className={'rounded-full w-full h-full object-cover border-8 border-[lightGray] ${imageFileUploadProgress && imageFileUploadProgress < 100 && "opacity-60"}'} />
+            </div>
+
+            {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
+
             <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username}/> 
             <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email}/> 
             <TextInput type='password' id='password' placeholder='Password'/>
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -19,7 +19,7 @@ export default function DashSidebar() {
         <Sidebar.Items>
             <Sidebar.ItemGroup>
                 <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item active= {tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                <Sidebar.Item active= {tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark' as='div'>
                     Profile
                 </Sidebar.Item>
                 </Link>
