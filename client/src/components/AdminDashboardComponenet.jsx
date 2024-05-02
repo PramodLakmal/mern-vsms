699bad798c20485a616f693b8a17753d1d2c5168
Sachin Prabuditha <sachinprabuditha@gmail.com>
2024-05-02 23:14:51 +0530

    Add recent users section to AdminDashboardComponenet.jsx

--- client/src/components/AdminDashboardComponenet.jsx
+++ client/src/components/AdminDashboardComponenet.jsx
@@ -156,6 +156,40 @@ export default function AdminDashboardComponenet() {
 
         </div>
 
+        <div className='flex flex-wrap gap-4 py-3 mx-auto justify-center'>
+        <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
+          <div className='flex justify-between  p-3 text-sm font-semibold'>
+            <h1 className='text-center p-2'>Recent users</h1>
+            <Button outline gradientDuoTone='pinkToOrange'>
+              <Link to={'/dashboard?tab=users'}>See all</Link>
+            </Button>
+          </div>
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>User image</Table.HeadCell>
+              <Table.HeadCell>Username</Table.HeadCell>
+            </Table.Head>
+            {users &&
+              users.map((user) => (
+                <Table.Body key={user._id} className='divide-y'>
+                  <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                    <Table.Cell>
+                      <img
+                        src={user.profilePicture}
+                        alt='user'
+                        className='w-10 h-10 rounded-full bg-gray-500'
+                      />
+                    </Table.Cell>
+                    <Table.Cell>{user.username}</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              ))}
+          </Table>
+        </div>
+      
+
+        
+
       <div className='flex flex-wrap gap-4 py-3 mx-auto justify-center'>
         <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
           <div className='flex justify-between  p-3 text-sm font-semibold'>
@@ -192,6 +226,7 @@ export default function AdminDashboardComponenet() {
               ))}
           </Table>
         </div> 
+      </div>
       </div>
     </div>
   )
