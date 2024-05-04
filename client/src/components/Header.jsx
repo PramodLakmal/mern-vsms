8bed9357adf8db983160ef67c5f137e08dcb41fa
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-05-04 12:27:42 +0530

    Fix formatting and styling issues in Header.jsx and Home.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,18 +4,17 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
-import {toggleTheme} from '../redux/theme/themeSlice';
-import {signoutSuccess} from '../redux/user/userSlice';
-
+import { toggleTheme } from '../redux/theme/themeSlice';
+import { signoutSuccess } from '../redux/user/userSlice';
 
 export default function Header() {
   const path = useLocation().pathname;
   const location = useLocation();
   const currentUser = useSelector((state) => state.user.currentUser);
   const dispatch = useDispatch();
-  const {theme} = useSelector(state => state.theme);
+  const { theme } = useSelector(state => state.theme);
 
-  const[searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState('');
   console.log(searchTerm);
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -25,7 +24,6 @@ export default function Header() {
     }
   }, [location.search]);
 
-
   const handleSignout = async () => {
     try {
       const res = await fetch('/api/user/signout', {
@@ -34,7 +32,7 @@ export default function Header() {
       const data = await res.json();
       if (!res.ok) {
         console.log(data.message);
-      }else {
+      } else {
         dispatch(signoutSuccess());
       }
     } catch (error) {
@@ -45,14 +43,20 @@ export default function Header() {
   return (
     <Navbar className="border-b-2 flex">
       <div>
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
-      >
-        <span className=" py-1">
-        <img src="https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/Logo%20and%20Other%2FSighe%20Auto.png?alt=media&token=95f727f5-dfdf-4d56-be9e-b4eabd8cf42d" alt="logo" />
-        </span></Link></div>
-      <form> 
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className=" py-1">
+            {theme === 'light' ? (
+              <img src="https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/Logo%20and%20Other%2FSighe%20Auto.png?alt=media&token=95f727f5-dfdf-4d56-be9e-b4eabd8cf42d" alt="logo" />
+            ) : (
+              <img src="https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/Logo%20and%20Other%2FSighe%20Auto%20dark.png?alt=media&token=b477e968-c373-42dd-ae91-51d9551c406e" alt="dark-mode-logo" />
+            )}
+          </span>
+        </Link>
+      </div>
+      <form>
         <TextInput
           type="text"
           placeholder="Search..."
@@ -64,43 +68,43 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={()=>dispatch(toggleTheme())}>
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={() => dispatch(toggleTheme())}>
           {theme === 'light' ? <FaMoon /> : <FaSun />}
         </Button>
         {currentUser ? (
           <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt='user' img={currentUser.profilePicture} rounded/>
-          }
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
           >
-          <Dropdown.Header>
-            <span className="block text-sm">
-              @{currentUser.username}
-            </span>
-            <span className="block text-sm font-medium truncate">
-              {currentUser.email}
-            </span>
-          </Dropdown.Header>
+            <Dropdown.Header>
+              <span className="block text-sm">
+                @{currentUser.username}
+              </span>
+              <span className="block text-sm font-medium truncate">
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
 
-          <Link to={'/dashboard?tab=profile'}>
-            <Dropdown.Item>
-              Profile
-            </Dropdown.Item>
-          </Link>
-          <Dropdown.Divider />
-          <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>
+                Profile
+              </Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
 
           </Dropdown>
-        ):
-        (
-          <Link to="/sign-in">
-          <Button gradientDuoTone="pinkToOrange" outline>
-            Sign In
-          </Button>
-        </Link>
-        )
+        ) :
+          (
+            <Link to="/sign-in">
+              <Button gradientDuoTone="pinkToOrange" outline>
+                Sign In
+              </Button>
+            </Link>
+          )
         }
         <Navbar.Toggle />
       </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,36 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div className='min-h-screen dark:text-white text-3xl font-semibold text-center mt-10'>
-      Home
-      <div >
-      <form className='flex border border-teal-500 rounded-md p-5 ml-80 mr-10 mt-60 size-fit'>
+    <div>
+    <div
+      className="h-screen w-full bg-cover bg-center"
+      style={{
+        backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/home%2Fhero-bg.jpg?alt=media&token=361eb655-5d3d-4caf-b093-ea85503457e2")`,
+      }}
+    >
+      <div className="flex h-full w-full items-center justify-center">
+        <div className="w-1/2 p-10">
+          <h1 className="text-4xl font-bold text-white mb-4">
+          Auto Maintenance & Repair Service
+          </h1>
+          <p className="text-lg text-white mb-8">
+            Get the best car service experience with our expert technicians and state-of-the-art equipment.
+          </p>
+          <button className="px-6 py-2 text-white bg-indigo-600 rounded-md shadow-md">
+            Show Services
+          </button>
+        </div>
+        <div className="w-1/2">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/home%2Fhero-banner.png?alt=media&token=5cb20325-2653-41e3-a61e-776cd0390bae"
+            alt="Car Service Center"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>      
+    </div>
+    <div >
+      <form className='flex border border-teal-500 rounded-md p-5 ml-auto mr-auto mt-10 mb-10 size-fit'>
       <h1 className='dark:text-white text-2xl font-semibold text-center ml-20'>Give Your Honnest Feedback Here</h1>
       <Link to={'/feedback'}>
       <Button type='button' size='lg' gradientDuoTone='purpleToBlue' className='ml-40 text-center'>
@@ -16,6 +42,8 @@ export default function Home() {
       </form>
         
       </div>
-      </div>
+
+
+    </div>
   )
 }
