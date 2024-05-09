36501974cc1252f1320a3768ba7ebf31455b5c00
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-05-09 08:19:20 +0530

    Update page titles and improve code structure

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,49 +1,65 @@
-import React from 'react'
-import {Button} from 'flowbite-react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
-    <div
-      className="h-screen w-full bg-cover bg-center"
-      style={{
-        backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/home%2Fhero-bg.jpg?alt=media&token=361eb655-5d3d-4caf-b093-ea85503457e2")`,
-      }}
-    >
-      <div className="flex h-full w-full items-center justify-center">
-        <div className="w-1/2 p-10">
-          <h1 className="text-4xl font-bold text-white mb-4">
-          Auto Maintenance & Repair Service
-          </h1>
-          <p className="text-lg text-white mb-8">
-            Get the best car service experience with our expert technicians and state-of-the-art equipment.
-          </p>
-          <button className="px-6 py-2 text-white bg-indigo-600 rounded-md shadow-md">
-            Show Services
-          </button>
+      <div
+        className="h-screen w-full bg-cover bg-center"
+        style={{
+          backgroundImage: `url("https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/home%2Fhero-bg.jpg?alt=media&token=361eb655-5d3d-4caf-b093-ea85503457e2")`,
+        }}
+      >
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="w-1/2 p-10">
+            <h1 className="text-4xl font-bold text-white mb-4">
+              Auto Maintenance & Repair Service
+            </h1>
+            <p className="text-lg text-white mb-8">
+              Get the best car service experience with our expert technicians
+              and state-of-the-art equipment.
+            </p>
+
+            <Link to="/services">
+              <button className="px-6 py-2 text-white bg-indigo-600 rounded-md shadow-md">
+                Show Services
+              </button>
+            </Link>
+          </div>
+          <div className="w-1/2">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/home%2Fhero-banner.png?alt=media&token=5cb20325-2653-41e3-a61e-776cd0390bae"
+              alt="Car Service Center"
+              className="h-full w-full object-cover"
+            />
+          </div>
         </div>
-        <div className="w-1/2">
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/home%2Fhero-banner.png?alt=media&token=5cb20325-2653-41e3-a61e-776cd0390bae"
-            alt="Car Service Center"
-            className="h-full w-full object-cover"
-          />
-        </div>
-      </div>      
-    </div>
-    <div >
-      <form className='flex border border-teal-500 rounded-md p-5 ml-auto mr-auto mt-10 mb-10 size-fit'>
-      <h1 className='dark:text-white text-2xl font-semibold text-center ml-20'>Give Your Honnest Feedback Here</h1>
-      <Link to={'/feedback'}>
-      <Button type='button' size='lg' gradientDuoTone='purpleToBlue' className='ml-40 text-center'>
-         Feedbacks
-        </Button> </Link>
-      </form>
-        
       </div>
-
-
+      <div>
+        <form className="flex border border-red-700 rounded-md p-5 ml-auto mr-auto mt-10 mb-10 size-fit">
+          <h1 className="dark:text-white text-2xl font-semibold text-center ml-20">
+            Give Your Honest Feedback Here
+          </h1>
+          <Link to={"/feedback"}>
+            <Button
+              type="button"
+              size="lg"
+              gradientDuoTone="purpleToBlue"
+              className="ml-40 text-center"
+            >
+              Feedbacks
+            </Button>{" "}
+          </Link>
+        </form>
+      </div>
+      <div className="w-full bg-gray-200 py-10">
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/mern-vsms.appspot.com/o/Logo%20and%20Other%2Fbaner.jpg?alt=media&token=c65b7dfd-f568-4c0b-8fc8-5fa3febec8e7"
+          alt="Banner"
+          className="mx-auto max-w-screen-xl"
+        />
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/Inventory.jsx
+++ client/src/pages/Inventory.jsx
@@ -17,7 +17,7 @@ export default function Inventory() {
   return (
     <div>
       <div className='flex flex-col gap-4 p-12 px-3 max-w-6xl mx-auto '>
-        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to our Inventory</h1>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to our Product Section...</h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
           Here you'll find a variety of products available in our inventory.
         </p>
@@ -25,12 +25,12 @@ export default function Inventory() {
           to='/search'
           className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'
         >
-          View all products
+          Find your products
         </Link>
       </div>
       
 
-      <div className='max-w-6xl mx-auto p-8 flex flex-col gap-8 py-1 items-center'>
+      <div className='max-w-6xl mx-auto p-8 flex flex-col gap-8 py-1 items-center mb-10'>
         {products && products.length > 0 && (
           <div className='flex flex-col gap-8'>
             <h2 className='text-2xl font-semibold text-center'>Recent products.....</h2>
